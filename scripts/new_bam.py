8ece8ce38a48ac311b4114b71878bf547eaea2b7
Arya Massarat <23412689+aryam7@users.noreply.github.com>
2020-07-31 13:48:47 -0700

    allow for filtering barcodes

--- scripts/new_bam.py
+++ scripts/new_bam.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import csv
 import pysam
 import argparse
 from pathlib import Path
@@ -12,16 +11,30 @@ def parse_format(fmt, f):
         fmt = 'b' if Path(f).suffix == '.bam' else 's'
     return fmt
 
+def tsv_reader(f):
+    """ read lines from a file """
+    for line in f:
+        yield line.split("\t")
+
 def read_barcodes(tsv):
     """ retrieve the tsv file containing the barcodes """
     with open(tsv) as infile:
         barcodes = {
-            rows[0]: rows[1]
-            for rows in csv.reader(infile, delimiter="\t")
+            row[0]: (row[1] if len(row)-1 and len(row[1]) else row[0])
+            for row in tsv_reader(infile)
         }
     return barcodes
 
-def main(barcodes, reads, in_format=None):
+def get_reads(reads):
+    """ get the reads but first get the tags from the first line"""
+    read_iter = iter(reads)
+    first = read_iter.next()
+    yield first.tags
+    yield first
+    for read in reads:
+        yield read
+
+def main(barcodes, reads, in_format=None, filter_also=False):
     """
         read the reads and alter their barcodes, returning each line as a
         generator function; the first line will be the pysam object
@@ -30,21 +43,23 @@ def main(barcodes, reads, in_format=None):
         in_format = ''
     reads = pysam.AlignmentFile(reads, "r"+in_format)
     yield reads
+    reads = get_reads(reads)
+    # get the indices of each tag for fast lookup later
     tag_idxs = {}
+    tags = next(reads)
+    for i in range(len(tags)):
+        if tags[i][0] == 'CB':
+            tag_idxs['CB'] = i
+    # iterate through each read
     for read in reads:
-        # initialize some variables
+        # initialize a pointer to the tags
         tags = read.tags
-        # read the indices of the tags into a dictionary for fast lookup
-        if not len(tag_idxs):
-            for i in range(len(tags)):
-                if tags[i][0] == 'CB':
-                    tag_idxs['CB'] = i
         # check to see whether the CB tag needs to be changed
         if tags[tag_idxs['CB']][1] in barcodes:
             # get the new CB tag
             tags[tag_idxs['CB']] = (tags[tag_idxs['CB']][0], barcodes[tags[tag_idxs['CB']][1]])
-        # apply the new tags
-        read.tags = tags
+        elif filter_also:
+            continue
         yield read
 
 def write_reads(out, reads, out_format=None):
@@ -63,21 +78,24 @@ if __name__ == "__main__":
         "-o", "--out", default="-", help="the filename to save the sam (or bam) to; defaults to stdout"
     )
     parser.add_argument(
-        "barcodes", type=Path, help="a TSV file containing two columns: 1) barcodes that should be changed, and 2) the new barcodes they should be changed to"
+        "barcodes", type=Path, help="a TSV file containing two columns: 1) barcodes in the input bam file, and 2) the new barcodes if they should be changed, otherwise leave the 2nd column out or leave it empty"
     )
     parser.add_argument(
         "reads", nargs="?", default="-", help="the filename from which to read the scRNA-seq reads from; defaults to stdin"
     )
     parser.add_argument(
-        "-i", "--in-format", choices=['s', 'b'], default=None, help="whether the reads are in the sam or bam file format; defaults to inferring from the file extension or if stdin, defaults to sam"
+        "--in-format", choices=['s', 'b'], default=None, help="whether the reads are in the sam or bam file format; defaults to inferring from the file extension or if stdin, defaults to sam"
     )
     parser.add_argument(
-        "-f", "--out-format", choices=['s', 'b'], default=None, help="whether the output reads should be written in the sam or bam file format; defaults to inferring from the file extension or if stdout, defaults to sam"
+        "--out-format", choices=['s', 'b'], default=None, help="whether the output reads should be written in the sam or bam file format; defaults to inferring from the file extension or if stdout, defaults to sam"
+    )
+    parser.add_argument(
+        "-f", "--filter-also", action='store_true', help="whether to discard reads whose barcodes don't appear in the barcodes file"
     )
     args = parser.parse_args()
 
     # parse the optional format args correctly
-    args.in_format, args.out_format = parse_format(args.in_format, args.reads), parse_format(args.out_format, args.out)
+    args.in_format, args.out_format = parse_format(args.in_format, args.reads, args.filter_also), parse_format(args.out_format, args.out)
 
     new_reads = write_reads(args.out, main(read_barcodes(args.barcodes), args.reads, args.in_format), args.out_format)
 
